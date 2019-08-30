44f864270fc477787f8e36458da24795e6465ad4
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2019-08-31 01:29:31 +0930

    v.012
    Added install script to be expanded
    Changed all files to use linux endings
    Added experimentation with usb enumeration in gps.py

--- comms/client/gps.py
+++ comms/client/gps.py
@@ -1,4 +1,48 @@
+#!/usr/bin/env python3
 #Nisputer
 #gps.py 
 #B Morgan
 #Receives GPS information
+
+import sys
+import glob
+import serial
+import serial.tools.list_ports
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+if __name__ == '__main__':
+    from serial.tools import list_ports
+    print(
+        "\n".join(
+            [
+                port.device + ': ' + port.hwid
+                for port in list_ports.ListPortInfo()
+            ]))
