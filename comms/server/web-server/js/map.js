037d395e515f26562ab278b5b8cccbc8a773fcc4
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2019-10-07 02:19:51 +1030

    Have test map working
    Adjusted layout, added some install scripts.

---
+++ comms/server/web-server/js/map.js
@@ -0,0 +1,12 @@
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([138.6, -34.9333333]),
+    zoom: 12
+  })
+});
