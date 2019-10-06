2a33e4a9ec63d34d05d6b491792ec10f2e6ddc87
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2019-10-07 02:19:51 +1030

    Have test map working
    Adjusted layout, added some install scripts.

---
+++ comms/server/install-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#install dependencies
+apt-get install npm -y
+apt-get installn nodejs -y
+
+#add as a service
--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-pip3 install pyserial
+
+echo "Please specify 0 to install as client, and 1 for server:"
+read choice
+
+
+if [[ $choice -eq 1 ]]; then
+	#install dependencies
+	cd comms/server/
+	./install-server.sh
+else
+	#install dependencies
+	apt-get install python3 -y
+	apt-get install python3-pip -y
+	pip3 install pyserial
+
+	#add as a service
+fi
