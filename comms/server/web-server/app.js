be1594807b2b877bddd9ed677eae8d14d1e1cbd7
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2019-12-15 14:19:27 +1030

    Integrated snyk to patch vulnerable node modules

--- comms/server/web-server/app.js
+++ comms/server/web-server/app.js
@@ -2,11 +2,31 @@ var port = 3434,
     express = require("express"),
     path = require('path'),
     parser = require("body-parser"),
-    app = express();
+    app = express(),
+    helmet = require('helmet'),
+    session = require('cookie-session');
 
 var long = 0.0;
 var lat = 0.0;
 var gpsRecvTime = new Date().getTime();
+
+app.use(helmet());
+app.set('trust proxy', 1); // trust first proxy
+
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+app.use(session(
+{
+    name: 'session',
+    keys: ['key1', 'key2'],
+    cookie:
+    {
+        secure: true,
+        httpOnly: true,
+        domain: 'example.com',
+        path: 'foo/bar',
+        expires: expiryDate
+    }
+}))
 
 app.use(parser.urlencoded(
 {
