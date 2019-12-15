f3f72cf7fb22da4ab1aa38a6b0c4349b964d9f8b
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2019-12-15 23:24:51 +1030

    Added login as well as logout functionality
    To the best of my ability, I have tried to make this as secure as possible.
    
    https://codeshack.io/basic-login-system-nodejs-express-mysql/
    
    I have followed this tutorial, and modified anything accordingly.

--- comms/server/install-server.sh
+++ comms/server/install-server.sh
@@ -2,6 +2,12 @@
 
 #install dependencies
 apt-get install npm -y
-apt-get installn nodejs -y
+apt-get install nodejs -y
+apt-get install mysql-server -y
+sudo service mysql restart
+
+cd web-server
+npm install
+cd ..
 
 #add as a service
--- comms/server/web-server/app.js
+++ comms/server/web-server/app.js
@@ -1,32 +1,43 @@
-var port = 3434,
-    express = require("express"),
-    path = require('path'),
-    parser = require("body-parser"),
-    app = express(),
-    helmet = require('helmet'),
-    session = require('cookie-session');
+var express = require("express");
+var session = require('express-session');
+var sanitizer = require('sanitizer');
+var parser = require("body-parser");
+var path = require('path');
+var mysql = require('mysql');
+var helmet = require('helmet');
 
+var app = express();
+
+//Create MYSQL connection
+var connection = mysql.createConnection(
+{
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'nisputer'
+});
+
+//Init Cookie and other packages
+app.use(session(
+{
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(parser.urlencoded({extended : true}));
+app.use(parser.json());
+
+var port = 3434;
 var long = 0.0;
 var lat = 0.0;
 var gpsRecvTime = new Date().getTime();
 
+//Login expiry
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+
 app.use(helmet());
 app.set('trust proxy', 1); // trust first proxy
 
-var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
-app.use(session(
-{
-    name: 'session',
-    keys: ['key1', 'key2'],
-    cookie:
-    {
-        secure: true,
-        httpOnly: true,
-        domain: 'example.com',
-        path: 'foo/bar',
-        expires: expiryDate
-    }
-}))
 
 app.use(parser.urlencoded(
 {
@@ -34,7 +45,61 @@ app.use(parser.urlencoded(
 }));
 app.get("/", function(req, res)
 {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    if (req.session.loggedin)
+    {
+        res.sendFile(path.join(__dirname + '/html/map.html'));
+    }
+    else
+    {        
+        res.sendFile(path.join(__dirname + '/html/login.html'));
+    }
+});
+
+app.post('/auth', function(req, res)
+{
+    var username = sanitizer.sanitize(req.body.username);
+    var password = sanitizer.sanitize(req.body.password);
+
+    if (username && password) {
+        var query = 'SELECT * FROM accounts WHERE username = ? AND password = ?';
+        connection.query(query, [username, password], function(error, results, fields)
+        {
+            if (results.length == 1)
+            {
+                req.session.loggedin = true;
+                req.session.username = username;
+                res.redirect('/map');
+            }
+            else
+            {
+                res.sendFile(path.join(__dirname + '/html/loginFail.html'));
+            }
+        });
+    }
+    else
+    {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
+});
+
+app.get("/map", function(req, res)
+{
+    if (req.session.loggedin)
+    {
+        res.sendFile(path.join(__dirname + '/html/map.html'));
+    }
+    else
+    {
+        res.send('Please login to view this page!');
+        res.end();
+    }
+});
+
+app.post("/logout", function(req, res)
+{
+    req.session.loggedin = false;
+    res.redirect("/");
 });
 
 app.get("/js/map.js", function(req, res)
